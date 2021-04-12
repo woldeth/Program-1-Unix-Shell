0e382e32670623f8b6967e4a51bf103f6050d592
Tomas <woldeth7@gmail.com>
2021-04-12 05:39:12 -0700

    MOVING ON TO HISTORY NOW

--- shell.c
+++ shell.c
@@ -14,45 +14,29 @@ void chomp(char *s)
     *s = 0;
 }
 
-void copy(char* org[], char* copy[]){
-
-    memmove(copy, org, MAX_LINE / 2 + 1);
-
-    printf("done \n");
-}
-
-void clear(char* org[]){
-
-    int i = 0;
-     while(org[i] != NULL){
-         org[i] = NULL;
-         i++;
-     }
-}
-
-int main(int argc, char **argv)
+int main()
 {
-    char *args[MAX_LINE / 2 + 1]; /* command line arguments */
-    char *cache[MAX_LINE / 2 + 1]; // cache for repeat
+    char *args[MAX_LINE]; /* command line arguments */
 
-    int should_run = 1;           /* flag to determine when to exit program */
+    int should_run = 1; /* flag to determine when to exit program */
 
     int count = 0;
 
-    int z = 0;
     while (should_run)
     {
+
+        //PROMPT THE USER
         printf("osh> ");
         fflush(stdout);
 
+        // TAKE INPUT
         char input[80];
         fgets(input, 80, stdin);
-
         char *ptr;
         ptr = strtok(input, " ");
 
+        // FORMAT INPUT TO CLEAR STRINGS
         int index = 0;
-
         while (ptr != NULL)
         {
             chomp(ptr);
@@ -61,24 +45,28 @@ int main(int argc, char **argv)
             ptr = strtok(NULL, " ");
         }
 
-
-
-        
-        
-        
+        // DETERMINE IF EXIT COMMAND OR REPEAT COMMAND 
         const char *e = "exit";
         const char *repeat = "!!";
-
-        if(strcmp(args[0], e) == 0){
+        if (strcmp(args[0], e) == 0)
+        {
             should_run = 0;
-            exit(0);
-        } else if(strcmp(args[0], repeat) == 0){
+            continue;
+        }
+        else if (strcmp(args[0], repeat) == 0)
+        {
             printf("repeat the process \n");
-            if(count == 0){
+            if (count == 0)
+            {
                 printf("ERROR -> You can not repeat command on first try");
-                exit(0);
-            }else{
-                copy(cache, args);
+                should_run = 0;
+                continue;
+            }
+            else
+            {
+                printf("PEFORM LAST COMMAND \n");
+                should_run = 0;
+                continue;
             }
         }
 
@@ -88,7 +76,7 @@ int main(int argc, char **argv)
 
         //(2) the child process will invoke execvp()
 
-        if (args[2] == NULL) // any value in third position
+        if (args[2] == NULL) // NO VALUE IN THIRD POSITION
         {
             if (child == 0)
             {
@@ -101,45 +89,27 @@ int main(int argc, char **argv)
                 wait(0); // parent waits
                 printf("* parent waited * \n");
                 count++;
-                copy(args, cache);
-                clear(args);
-                input[0] =0;
-
-                // int i = 0;
-                // while(input[i] != '\0'){
-                //  input[i] = '\0';
-                //     i++;
-                // }
-              
-    
             }
         }
-        else if(*args[2] == '&')
-        {
-            if (child == 0)
-            {
-                args[2] = NULL;
-                execvp(args[0], args); // child process performs the command
-                exit(0);
-            }
-            else
-            {
+        // else if(*args[2] == '&')
+        // {
+        //     if (child == 0)
+        //     {
+        //         args[2] = NULL;
+        //         execvp(args[0], args); // child process performs the command
+        //         exit(0);
+        //     }
+        //     else
+        //     {
 
-                sleep(1);
-                printf("* running concurently * \n");
-                count++;
-                copy(args, cache);
-                clear(args);
+        //         sleep(1);
+        //         printf("* running concurently * \n");
+        //         count++;
 
-                int i = 0;
-                while(input[i] != '\0'){
-                 input[i] = '\0';
-                    i++;
-                }
-                
+        //     }
+        // }
 
-            }
-        }
+        //printf("debug");
     }
 
     return 0;
